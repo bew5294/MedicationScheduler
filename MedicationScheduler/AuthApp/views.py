0628a1c46d7d61d5dd646a5a4044cdc68f82e820
kdgromova <kdgromova@gmail.com>
2021-12-26 19:18:58 -0500

    added initial registration functionality

--- MedicationScheduler/AuthApp/views.py
+++ MedicationScheduler/AuthApp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from AuthApp.forms import RegistrationForm
+
 
 # Create your views here.
+
+def registration_view(request):
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('form')
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            return redirect("http://127.0.0.1:8000/")
+        else:
+            context['registration_form'] = form
+    else:  # GET request
+        form = RegistrationForm()
+        context['registration_form'] = form
+    return render(request, 'register.html', context)
--- MedicationScheduler/MedicationScheduler/urls.py
+++ MedicationScheduler/MedicationScheduler/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AuthApp.views import registration_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', registration_view, name="register")
 ]
