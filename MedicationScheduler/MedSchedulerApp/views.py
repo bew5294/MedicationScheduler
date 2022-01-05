f2007df4a863a48d117d554457bb1abeb69aa727
kdgromova <kdgromova@gmail.com>
2022-01-05 16:26:31 -0500

    added pop-ups for events for displaying additional information

--- MedicationScheduler/MedSchedulerApp/views.py
+++ MedicationScheduler/MedSchedulerApp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
+import json
 
 
 # Create your views here.
 def home_view(request):
+    events = [{"id": 'a', "title": 'my event', "start": "2022-01-06", "end": "2022-01-08", "description": 'This is a cool event'}, {"id": 'b', "title": 'my event 2', "start": "2022-01-08", "description": 'This is a cool event 2'}]
     if not request.user.is_authenticated:
         return redirect('Auth:login')
-    return render(request, 'home.html', {"user": request.user})
+    return render(request, 'home.html', {"user": request.user, "events": json.dumps(events)})
 
 
 def front_view(request):
