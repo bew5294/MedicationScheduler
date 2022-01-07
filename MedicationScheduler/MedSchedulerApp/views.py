19411b28e9714cde2604573f3135a07977bfc468
kdgromova <kdgromova@gmail.com>
2022-01-07 12:28:10 -0500

    added some comments

--- MedicationScheduler/MedSchedulerApp/views.py
+++ MedicationScheduler/MedSchedulerApp/views.py
@@ -4,6 +4,7 @@ import json
 
 # Create your views here.
 def home_view(request):
+    # example
     events = [{"id": 'a', "title": 'my event', "start": "2022-01-06", "end": "2022-01-08", "description": 'This is a cool event'}, {"id": 'b', "title": 'my event 2', "start": "2022-01-08", "description": 'This is a cool event 2'}]
     if not request.user.is_authenticated:
         return redirect('Auth:login')
